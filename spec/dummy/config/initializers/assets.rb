fbfc7de4909c7c0386d82a9c46b31e407fd866f0
john.henderson <john.henderson@creativetrust.com>
2016-01-07 17:39:35 -0600

    Multiple changes to the dummy project to get tests functioning, and updates read me

---
+++ spec/dummy/config/initializers/assets.rb
@@ -0,0 +1,2 @@
+Rails.application.config.assets.precompile += %w( outdatedbrowser/require_outdatedbrowser.js )
+Rails.application.config.assets.precompile += %w( outdatedbrowser/require_outdatedbrowser.css )
